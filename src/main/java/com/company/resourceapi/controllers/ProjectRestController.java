f700ee2dbdf95dfd98a25b721013c29659276590
Itunuloluwa Fatoki <itunuworks@gmail.com>
2020-07-06 15:45:14 +0100

    albalaablalalaj

--- src/main/java/com/company/resourceapi/controllers/ProjectRestController.java
+++ src/main/java/com/company/resourceapi/controllers/ProjectRestController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,4 +39,14 @@ public class ProjectRestController {
 	) {
 		return projectService.getProject(projectId);
 	}
+
+	@ApiOperation("Create a Project")
+	@PostMapping("/")
+	public Project createProject(
+		@RequestBody Project project
+	) {
+		System.out.println(project);
+		return projectService.createProject(project);
+	}
+	
 }
--- src/main/java/com/company/resourceapi/services/impl/ProjectServiceImpl.java
+++ src/main/java/com/company/resourceapi/services/impl/ProjectServiceImpl.java
@@ -1,8 +1,12 @@
 package com.company.resourceapi.services.impl;
 
+import java.util.Optional;
+
 import com.company.resourceapi.entities.Project;
+import com.company.resourceapi.entities.SdlcSystem;
 import com.company.resourceapi.exceptions.NotFoundException;
 import com.company.resourceapi.repositories.ProjectRepository;
+import com.company.resourceapi.repositories.SdlcSystemRepository;
 import com.company.resourceapi.services.ProjectService;
 
 import lombok.Data;
@@ -15,6 +19,7 @@ import org.springframework.stereotype.Service;
 public class ProjectServiceImpl implements ProjectService {
 
 	private final ProjectRepository projectRepository;
+	private final SdlcSystemRepository sdlcSystemRepository;
 
 	public Project getProject(long id) {
 		try {
@@ -23,5 +28,14 @@ public class ProjectServiceImpl implements ProjectService {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	@Override
+	public Project createProject(Project project) {
+		// TODO Auto-generated method stub
+		long id = project.getSdlcSystem().getId();
+		SdlcSystem sdlcSystem = sdlcSystemRepository.findById(id).get();
+		project.setSdlcSystem(sdlcSystem);
+		return projectRepository.save(project);
 	}
 }
